5e15fdb9e9d8f166e5be5f9213bdf2f6ec304d51
ImprovingTyler <TylerJamesLundin@gmail.com>
2022-03-02 21:39:29 -0800

    HashRouter instead of BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import store from './app/store'
 import { Provider } from 'react-redux'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, HashRouter} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename='/'>
+      <HashRouter>
         <App />
-      </BrowserRouter>  
+      </HashRouter>  
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
